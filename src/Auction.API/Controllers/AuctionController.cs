f7863bc9a17153362402db8ba4e2c0e2d12d42f5
Marcos Venicio <marcos.venicio1230@hotmail.com>
2024-02-06 09:04:24 -0300

    Removendo Controller padrão e add AuctionController

---
+++ src/Auction.API/Controllers/AuctionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Auction.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuctionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCurrentAuction()
+        {
+            return Ok("Marcos Venicio");
+        }
+    }
+}
